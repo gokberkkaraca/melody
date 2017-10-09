752e9542620ab8d7a2f5b1de1fb7f48947972c07
Gokberk Karaca <gskaraca1905@gmail.com>
2017-10-09 09:49:59 +0200

    Add new SignUpActivity, add functinoality to Create New Account button

--- app/src/main/java/ch/epfl/sweng/melody/LoginActivity.java
+++ app/src/main/java/ch/epfl/sweng/melody/LoginActivity.java
@@ -1,7 +1,9 @@
 package ch.epfl.sweng.melody;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void signUp(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
