eeac824f7ca7d19454c94f079fab45976391493a
Jiacheng Xu <xjcmaxwell@163.com>
2017-10-10 22:05:36 +0200

    Implemented displaying picture which be chosen from camera and gallery in a imageView.

--- app/src/main/java/ch/epfl/sweng/melody/CreateMemoryActivity.java
+++ app/src/main/java/ch/epfl/sweng/melody/CreateMemoryActivity.java
@@ -1,39 +1,39 @@
 package ch.epfl.sweng.melody;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class CreateMemoryActivity extends AppCompatActivity {
+    private ImageView imageView;
+    public final int SELECT_FILE = 1;
+    public final int REQUEST_CAMERA =2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_memory);
+        imageView = (ImageView) findViewById(R.id.display_chosen_photo);
     }
 
-    private void accessCamera() {
-        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivity(intent);
-    }
-
-    private void accessGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, 1);
-    }
-
-    public void pickPhotoDialog(View view){
-        final CharSequence[] options = { "Take Photo", "Choose from Library", "Cancel" };
+    public void pickPhotoDialog(View view) {
+        final CharSequence[] options = {"Take Photo", "Choose from Library", "Cancel"};
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Add photo");
-        builder.setItems(options, new DialogInterface.OnClickListener(){
+        builder.setItems(options, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int option){
-                if (options[option].equals("Take Photo")){
+            public void onClick(DialogInterface dialog, int option) {
+                if (options[option].equals("Take Photo")) {
                     accessCamera();
                 } else if (options[option].equals("Choose from Library")) {
                     accessGallery();
@@ -43,5 +43,45 @@ public class CreateMemoryActivity extends AppCompatActivity {
             }
         });
         builder.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCameraResult(data);
+        }
+    }
+
+    private void accessCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,REQUEST_CAMERA);
+    }
+
+    private void accessGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select File"),SELECT_FILE);
+    }
+
+    private void onGalleryResult(Intent data) {
+        Bitmap bm=null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        imageView.setImageBitmap(bm);
+    }
+
+    private void onCameraResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        imageView.setImageBitmap(thumbnail);
     }
 }
