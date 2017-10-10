f6a54aa92e896d2c54bedbeb61f4be7c9188a39b
Jiacheng Xu <xjcmaxwell@163.com>
2017-10-10 18:14:00 +0200

    Add a alert dialog for 'take photo' and 'choose from album'

--- app/src/main/java/ch/epfl/sweng/melody/CreateMemoryActivity.java
+++ app/src/main/java/ch/epfl/sweng/melody/CreateMemoryActivity.java
@@ -1,7 +1,9 @@
 package ch.epfl.sweng.melody;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,13 +16,32 @@ public class CreateMemoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_memory);
     }
 
-    public void accessCamera(View view){
+    private void accessCamera() {
         Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         startActivity(intent);
     }
 
-    public void accessGallery(View view){
+    private void accessGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent,1);
+        startActivityForResult(intent, 1);
+    }
+
+    public void pickPhotoDialog(View view){
+        final CharSequence[] options = { "Take Photo", "Choose from Library", "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add photo");
+        builder.setItems(options, new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int option){
+                if (options[option].equals("Take Photo")){
+                    accessCamera();
+                } else if (options[option].equals("Choose from Library")) {
+                    accessGallery();
+                } else if (options[option].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
     }
 }
