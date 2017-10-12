c5218c4ac99541a24479d339b69fcf6624260d1e
Gokberk Karaca <gskaraca1905@gmail.com>
2017-10-12 09:00:40 +0200

    Add timer to MainActivity

--- app/src/main/java/ch/epfl/sweng/melody/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/melody/MainActivity.java
@@ -1,13 +1,24 @@
 package ch.epfl.sweng.melody;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }, 2000); // 4 seconds
     }
 }
