b8b5add1126cea052cf84f777220c51e537df459
Gokberk Karaca <gskaraca1905@gmail.com>
2017-10-12 17:50:51 +0200

    Display username and user profile picture on UserProfileActivity

--- app/src/main/java/ch/epfl/sweng/melody/UserProfileActivity.java
+++ app/src/main/java/ch/epfl/sweng/melody/UserProfileActivity.java
@@ -2,7 +2,13 @@ package ch.epfl.sweng.melody;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import ch.epfl.sweng.melody.memory.GoogleProfilePictureAsync;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -10,6 +16,12 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        username.setText(LoginActivity.GOOGLE_ACCOUNT.getDisplayName());
+
+        ImageView profilePicView = (ImageView) findViewById(R.id.profilePicView);
+        new GoogleProfilePictureAsync(profilePicView, LoginActivity.GOOGLE_ACCOUNT.getPhotoUrl()).execute();
     }
 }
 
