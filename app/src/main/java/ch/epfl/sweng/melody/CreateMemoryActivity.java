678944a2a907a39abc45d89ae4f0197e5cea0e4c
Jiacheng Xu <xjcmaxwell@163.com>
2017-10-19 10:31:53 +0200

    Optimize the import

--- app/src/main/java/ch/epfl/sweng/melody/CreateMemoryActivity.java
+++ app/src/main/java/ch/epfl/sweng/melody/CreateMemoryActivity.java
@@ -7,24 +7,18 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.VideoView;
 
-import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class CreateMemoryActivity extends AppCompatActivity {
     private static final int REQUEST_PHOTO_GALLERY = 1;
--- app/src/main/java/ch/epfl/sweng/melody/account/GoogleAccount.java
+++ app/src/main/java/ch/epfl/sweng/melody/account/GoogleAccount.java
@@ -1,14 +1,11 @@
 package ch.epfl.sweng.melody.account;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.GoogleApiClient;
-
-import ch.epfl.sweng.melody.user.User;
 
 
 public class GoogleAccount {
