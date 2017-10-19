ba1121bf9fb7ed7982d7fb740c572a51e16a60ee
Gokberk Karaca <gskaraca1905@gmail.com>
2017-10-19 17:29:55 +0200

    Optimize code, optimize imports

--- app/src/main/java/ch/epfl/sweng/melody/CreateMemoryActivity.java
+++ app/src/main/java/ch/epfl/sweng/melody/CreateMemoryActivity.java
@@ -35,7 +35,6 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
