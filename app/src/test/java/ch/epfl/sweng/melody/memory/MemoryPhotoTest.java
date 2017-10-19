8b1fe0121bfd7e49d7bece56b03ecac75e864761
Gokberk Karaca <gskaraca1905@gmail.com>
2017-10-19 21:39:33 +0200

    Test MemoryPhoto class

--- app/src/test/java/ch/epfl/sweng/melody/memory/MemoryPhotoTest.java
+++ app/src/test/java/ch/epfl/sweng/melody/memory/MemoryPhotoTest.java
@@ -1,8 +1,36 @@
+package ch.epfl.sweng.melody.memory;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by Gökberk Karaca on 10/19/2017.
- */
 public class MemoryPhotoTest {
 
+    private String author;
+    private String text;
+    private String location;
+    private List<String> photos;
+    private MemoryPhoto memoryPhoto;
+
+    @Before
+    public void createMemoryAudio() throws Exception {
+        author = UUID.randomUUID().toString();
+        text = "Example test";
+        location = "Switzerland, Lausanne";
+        photos = new ArrayList<>();
+        photos.add("http://www.google.com");
+        photos.add("http://www.twitter.com");
+        photos.add("http://www.facebook.com");
+        memoryPhoto = new MemoryPhoto(author, text, location, photos);
+    }
+
+    @Test
+    public void getPhotos() throws Exception {
+        assertTrue(memoryPhoto.getPhotos().equals(photos));
+    }
 }
