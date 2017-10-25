bd658d19b05fe0d36d584c5b1c6ceb6050befa43
Yusi Zou <yusi.zou@epfl.ch>
2017-10-25 16:58:49 +0200

    optimize imports

--- app/src/androidTest/java/ch/epfl/sweng/melody/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/melody/MainActivityTest.java
@@ -1,28 +1,11 @@
 package ch.epfl.sweng.melody;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import ch.epfl.sweng.melody.user.User;
-
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-/**
- * Created by yusiz on 2017/10/25.
- */
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
