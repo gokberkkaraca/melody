2549b2397a7b7b1e998764815176e088056df609
Maxime Lemarignier <maxime.lemarignier@epfl.ch>
2017-11-01 11:20:14 +0100

    working but not for all videos

--- app/src/main/java/ch/epfl/sweng/melody/memory/MemoryAdapter.java
+++ app/src/main/java/ch/epfl/sweng/melody/memory/MemoryAdapter.java
@@ -1,8 +1,10 @@
 package ch.epfl.sweng.melody.memory;
 
 import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -17,6 +19,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -61,21 +64,22 @@ public class MemoryAdapter extends RecyclerView.Adapter<MemoryAdapter.MemoriesVi
                 holder.author.setText(user.getDisplayName());
                 new GoogleProfilePictureAsync(holder.authorPic, Uri.parse(user.getProfilePhotoUrl())).execute();
 
-                //-------------------------------This method should fetch the photo but android won't cast it to MemoryPhoto-----------------
+                /*
                 if (memory.getPhotoUrl() != null) {
                     Picasso.with(holder.itemView.getContext()).load(memory.getPhotoUrl()).into(holder.memoryPic);
-                }
+                }*/
 
                 //--------------------------------------This will work but we need to fix the getMemoryType() method first------------------------
-                /*if (memory.getMemoryType() == Memory.MemoryType.PHOTO) {
-                    Picasso.with(holder.itemView.getContext()).load(memory.getPhoto()).into(holder.memoryPic);
-                    holder.description.setText("THIS IS A PHOTO"); //don't fordet to delete this
+                if (memory.getMemoryType() == Memory.MemoryType.PHOTO) {
+                    Picasso.with(holder.itemView.getContext()).load(memory.getPhotoUrl()).into(holder.memoryPic);
+                    //holder.description.setText("THIS IS A PHOTO");
                 }
                 else if (memory.getMemoryType() == Memory.MemoryType.VIDEO) {
-                    Bitmap thumbnail = ThumbnailUtils.createVideoThumbnail(memory.getVideoUrl() , MediaStore.Video.Thumbnails.MICRO_KIND);
+                    //Bitmap thumbnail = ThumbnailUtils.createVideoThumbnail(memory.getVideoUrl() , MediaStore.Video.Thumbnails.MICRO_KIND);
+                    Bitmap thumbnail =retrieveVideoFrameFromVideo(memory.getVideoUrl());
                     holder.memoryPic.setImageBitmap(thumbnail);
-                    holder.description.setText("THIS IS A VIDEO"); //don't fordet to delete this
-                }*/
+                    //holder.description.setText("THIS IS A VIDEO");
+                }
                 //---------------------------------------------------------------------------------------------------------------------------------
             }
 
@@ -106,5 +110,22 @@ public class MemoryAdapter extends RecyclerView.Adapter<MemoryAdapter.MemoriesVi
             memoryPic = view.findViewById(R.id.memoryPic);
         }
     }
+
+    //------------------------
+
+    public static Bitmap retrieveVideoFrameFromVideo(String videoPath)
+    {
+        Bitmap bitmap = null;
+        MediaMetadataRetriever mediaMetadataRetriever = new MediaMetadataRetriever();
+        mediaMetadataRetriever.setDataSource(videoPath, new HashMap<String, String>());
+        bitmap = mediaMetadataRetriever.getFrameAtTime();
+
+        if (mediaMetadataRetriever != null) {
+            mediaMetadataRetriever.release();
+        }
+
+        return bitmap;
+    }
+    //--------------------------
 
 }
