15850bb4408bd36259581f1f87440d5622547fad
karunyatota <kay.tota@gmail.com>
2017-11-01 19:47:23 +0100

    Fixing more lint issues and test coverage

---
+++ app/src/androidTest/java/ch/epfl/sweng/melody/PublicMemoryActivityTest.java
@@ -0,0 +1,114 @@
+package ch.epfl.sweng.melody;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+
+import ch.epfl.sweng.melody.user.User;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PublicMemoryActivityTest {
+    private final String defaultProfilePhotoUrl = "https://firebasestorage.googleapis.com/v0/b/firebase-melody.appspot.com/o/user_profile%2Fdefault_profile.png?alt=media&token=0492b3f5-7e97-4c87-a3b3-f7602eb94abc";
+
+    @Rule
+    public final IntentsTestRule<PublicMemoryActivity> publicMemoryActivityIntentsTestRule =
+            new IntentsTestRule<PublicMemoryActivity>(PublicMemoryActivity.class) {
+                @Override
+                protected Intent getActivityIntent() {
+                    User user;
+                    final GoogleSignInAccount googleSignInAccount = mock(GoogleSignInAccount.class);
+                    when(googleSignInAccount.getId()).thenReturn("QWERTYU");
+                    when(googleSignInAccount.getGivenName()).thenReturn("Jiacheng");
+                    when(googleSignInAccount.getFamilyName()).thenReturn("Xu");
+                    when(googleSignInAccount.getDisplayName()).thenReturn("Jiacheng Xu");
+                    when(googleSignInAccount.getEmail()).thenReturn("xjcmaxwell@163.com");
+                    when(googleSignInAccount.getPhotoUrl()).thenReturn(Uri.parse(defaultProfilePhotoUrl));
+                    user = new User(googleSignInAccount);
+                    Context targetContext = InstrumentationRegistry.getInstrumentation()
+                            .getTargetContext();
+                    Intent intent = new Intent(targetContext, PublicMemoryActivity.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("USER", user);
+                    intent.putExtras(bundle);
+                    return intent;
+                }
+            };
+
+    @Test
+    public void goToPublicMemoryTest() throws Exception{
+        onView(withId(R.id.home)).check(matches(allOf( isEnabled(), isClickable()))).perform(
+                new ViewAction() {
+                    @Override
+                    public Matcher<View> getConstraints() {
+                        return ViewMatchers.isEnabled(); // no constraints, they are checked above
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return "click public memory feed button";
+                    }
+
+                    @Override
+                    public void perform(UiController uiController, View view) {
+                        view.performClick();
+                    }
+                }
+        );
+        intended(hasComponent(PublicMemoryActivity.class.getName()));
+    }
+
+    @Test
+    public void goToCreateNewMemoryTest() throws Exception{
+        onView(withId(R.id.plus)).perform(click());
+        intended(hasComponent(CreateMemoryActivity.class.getName()));
+    }
+
+    @Test
+    public void goToUserProfileTest() throws Exception{
+        onView(withId(R.id.person)).check(matches(allOf( isEnabled(), isClickable()))).perform(
+                new ViewAction() {
+                    @Override
+                    public Matcher<View> getConstraints() {
+                        return ViewMatchers.isEnabled(); // no constraints, they are checked above
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return "click user profile button";
+                    }
+
+                    @Override
+                    public void perform(UiController uiController, View view) {
+                        view.performClick();
+                    }
+                }
+        );
+        intended(hasComponent(UserProfileActivity.class.getName()));
+    }
+
+}
+
