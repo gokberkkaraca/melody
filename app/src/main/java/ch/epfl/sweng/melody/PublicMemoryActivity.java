b33c5c25dea767245b9b66366feca277df115f75
Maxime Lemarignier <maxime.lemarignier@epfl.ch>
2017-11-15 18:24:51 +0100

    Almost working

--- app/src/main/java/ch/epfl/sweng/melody/PublicMemoryActivity.java
+++ app/src/main/java/ch/epfl/sweng/melody/PublicMemoryActivity.java
@@ -2,19 +2,29 @@ package ch.epfl.sweng.melody;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import ch.epfl.sweng.melody.database.DatabaseHandler;
@@ -23,22 +33,28 @@ import ch.epfl.sweng.melody.memory.MemoryAdapter;
 import ch.epfl.sweng.melody.user.User;
 import ch.epfl.sweng.melody.util.MenuButtons;
 
-public class PublicMemoryActivity extends Activity {
+public class PublicMemoryActivity extends FragmentActivity { //was extending Activity before
 
     private static User user;
     private List<Memory> memoryList;
     private RecyclerView recyclerView;
-    private MemoryAdapter memoryAdapter;
+    private static MemoryAdapter memoryAdapter;
+    private static Button dateButton;
+    private static long memoryStartTime = 0L;
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
+    private static boolean datePicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_public_memory);
 
+        user = (User) getIntent().getExtras().getSerializable(MainActivity.USER_INFO);
+        dateButton = (Button)findViewById(R.id.dateButton);
+
         memoryList = new ArrayList<>();
         fetchMemoriesFromDatabase();
 
-        user = (User) getIntent().getExtras().getSerializable(MainActivity.USER_INFO);
     }
 
     private void fetchMemoriesFromDatabase() {
@@ -48,7 +64,9 @@ public class PublicMemoryActivity extends Activity {
                 for (DataSnapshot memDataSnapshot : dataSnapshot.getChildren()) {
                     Memory memory = memDataSnapshot.getValue(Memory.class);
                     assert memory != null;
-                    memoryList.add(memory);
+                    if(memory.getLongId() > memoryStartTime) {
+                        memoryList.add(memory);
+                    }
                 }
 
                 memoryAdapter = new MemoryAdapter(memoryList);
@@ -105,4 +123,35 @@ public class PublicMemoryActivity extends Activity {
     public void goToUser(View view) {
         MenuButtons.goToUserProfileActivity(this, user);
     }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            datePicked = true;
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(year, month, day, 0, 0, 0);
+            memoryStartTime = Long.MAX_VALUE - calendar.getTimeInMillis();
+            dateButton.setText(dateFormat.format(calendar.getTime()));
+            //this.recreate();
+        }
+    }
+
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
 }
