8e382123168c4d14b83c878f3fb43c13ee322985
Gökberk Karaca <gskaraca1905@gmail.com>
2017-11-22 17:51:06 +0100

    Create needed fields in UserContactInfo to protect give it to other users

--- app/src/main/java/ch/epfl/sweng/melody/user/UserContactInfo.java
+++ app/src/main/java/ch/epfl/sweng/melody/user/UserContactInfo.java
@@ -1,9 +1,32 @@
 package ch.epfl.sweng.melody.user;
 
-/**
- * Created by gokbe on 11/22/2017.
- */
+class UserContactInfo {
 
-public class UserContactInfo {
+    private String userId;
+    private String displayName;
+    private String profilePhotoUrl;
+    private String email;
 
+    protected UserContactInfo(String userId, String displayName, String profilePhotoUrl, String email) {
+        this.userId = userId;
+        this.displayName = displayName;
+        this.profilePhotoUrl = profilePhotoUrl;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getProfilePhotoUrl() {
+        return profilePhotoUrl;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
