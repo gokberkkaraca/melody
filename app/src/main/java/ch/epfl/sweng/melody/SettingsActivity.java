4157953aa548ecb276d77a12d36adc8b63f364ce
Yusi Zou <yusi.zou@epfl.ch>
2017-11-27 01:07:14 +0100

    Add Setting Avtivity, minRadius, maxRadius, themeColor

--- app/src/main/java/ch/epfl/sweng/melody/SettingsActivity.java
+++ app/src/main/java/ch/epfl/sweng/melody/SettingsActivity.java
@@ -1,17 +1,50 @@
 package ch.epfl.sweng.melody;
 
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.util.List;
+
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
-
-
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
     }
+
+    public static class SettingsFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+//    @Override
+//    public void onBuildHeaders(List target) {
+//        super.onBuildHeaders(target);
+//        loadHeadersFromResource(R.xml.preferences, target);
+//    }
+//
+//    @Override
+//    protected boolean isValidFragment(String fragmentName) {
+//        Log.d("DEBUG", "isValidFragment, fragmentName: " + fragmentName);
+//        return true;
+//    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        // Display the fragment as the main content.
+//        getFragmentManager().beginTransaction()
+//                .replace(android.R.id.content, new SettingsFragment())
+//                .commit();
+//    }
 }
