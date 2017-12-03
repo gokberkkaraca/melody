8dd6538d7748d2dabca01324d52b5e688540597d
Jiacheng Xu <xjcmaxwell@163.com>
2017-12-03 20:32:52 +0100

    Add tests for the ResetPasswordActivity.

--- app/src/main/java/ch/epfl/sweng/melody/ResetPasswordActivity.java
+++ app/src/main/java/ch/epfl/sweng/melody/ResetPasswordActivity.java
@@ -42,7 +42,7 @@ public class ResetPasswordActivity extends AppCompatActivity implements View.OnC
         String email = inputEmail.getText().toString().trim();
 
         if (TextUtils.isEmpty(email)) {
-            Toast.makeText(getApplication(), "Enter your registered email!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplication(), R.string.reset_password_empty, Toast.LENGTH_SHORT).show();
             return;
         }
 
@@ -52,9 +52,9 @@ public class ResetPasswordActivity extends AppCompatActivity implements View.OnC
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(ResetPasswordActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(ResetPasswordActivity.this, R.string.send_reset_password_email, Toast.LENGTH_SHORT).show();
                         } else {
-                            Toast.makeText(ResetPasswordActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(ResetPasswordActivity.this, R.string.fail_send_rest_password_email, Toast.LENGTH_SHORT).show();
                         }
                         progressBar.setVisibility(View.GONE);
                     }
