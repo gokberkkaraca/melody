682bdee4889e00d13889843182095e1f83d5b2da
Gökberk Karaca <gskaraca1905@gmail.com>
2017-12-07 17:29:36 +0100

    Test FriendListActivity

---
+++ app/src/androidTest/java/ch/epfl/sweng/melody/FriendListActivityTest.java
@@ -0,0 +1,30 @@
+package ch.epfl.sweng.melody;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class FriendListActivityTest {
+
+    @Rule
+    public final IntentsTestRule<FriendListActivity> memoryDetailActivityIntentsTestRule =
+            new IntentsTestRule<FriendListActivity>(FriendListActivity.class, false, true) {
+                @Override
+                protected Intent getActivityIntent() {
+                    Context targetContext = InstrumentationRegistry.getInstrumentation()
+                            .getTargetContext();
+                    Intent intent = new Intent(targetContext, DetailedMemoryActivity.class);
+                    intent.putExtra(PublicMemoryActivity.EXTRA_GOING_TO_USER_LIST, "all");
+                    return intent;
+                }
+            };
+
+    @Test
+    public void testCanPass(){
+
+    }
+}
